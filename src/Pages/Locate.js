62936139eccfc0a9b63fa4e96ff2f24c247bb37b
Ariel Uy <auy@ibm.com>
2020-07-15 14:53:03 -0400

    Create search bar, make API request, display results

--- src/Pages/Locate.js
+++ src/Pages/Locate.js
@@ -2,7 +2,12 @@ import React, { Component } from 'react'
 import logo from './../logo.svg';
 import Alerts from './../Components/Alerts'
 import Button from 'react-bootstrap/Button'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Card from 'react-bootstrap/Card'
 import Link from 'react-router-dom/Link'
+import { Form, CardDeck } from 'react-bootstrap';
+import truck_img from '../truck.jpg';
 
 
 class Locate extends Component {
@@ -11,24 +16,105 @@ class Locate extends Component {
         console.log("On Locate Page now")
     }
 
+    state = {
+        product: null
+    }
+
+    handleSearchSubmit(event) {
+        event.preventDefault();
+        const form = event.currentTarget;
+        const barcode_id = form.elements[0].value;
+        console.log("got barcode!");
+        console.log(barcode_id);
+        this.search(barcode_id)
+    }
+
+    search(barcode_id) {
+       // Go to this url and get the response 
+        fetch(`http://127.0.0.1:5000/v1/product?barcode_id=${barcode_id}`, {
+            mode: 'cors',
+            method: "GET",
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Request-Headers": "*",
+                "Access-Control-Allow-Headers": "*"
+            }
+        }).then(res => res.json()) // then convert to json 
+        .then( // then do somethin with result or the error 
+            (result) => {
+                console.log("We heard something!!")
+                console.log(result)
+                this.setState({ product: result })
+            },
+            (error) => {
+                console.log(error);
+                console.log("ERROR!")
+            }
+        )
+        this.render()
+    }
+
     render() {
+        const product = this.state.product;
         // contains the default react project imagery 
         return (
         <div className="App">
-            <Alerts />
             <Link to="/">
                 <Button variant="primary">Take me Home!</Button>
             </Link>
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <h1>Welcome to the Future of IBM Energy Trust</h1>
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                    Learn React
-                </a>
-            </header>
+                <form onSubmit={e => this.handleSearchSubmit(e)}>
+                    <Row>
+                        <Col xs="auto">
+                            <Form.Group controlId="barcode">
+                                <Form.Control type="input" placeholder="Barcode" />
+                            </Form.Group>
+                        </Col>
+                        <Col xs="auto">
+                            <Button variant="primary" type="submit" block>
+                                Search
+                            </Button>
+                        </Col>
+                    </Row>
+                </form>
+                {product != null && product != undefined ? 
+                    <div className="container">
+                        <span>{product[0].category}</span>
+                        <CardDeck>
+                        <Card style={{ width: '18rem' }} text='dark'>
+                        <Card.Img variant="top" src={truck_img} />
+                        <Card.Body>
+                            <Card.Title>Production</Card.Title>
+                            <Card.Text>
+                            CO2: {product[0].rating_data.CO2}
+                            </Card.Text>
+                            <Button variant="primary">Learn more</Button>
+                        </Card.Body>
+                        </Card>
+
+                        <Card style={{ width: '18rem' }} text='dark'>
+                        <Card.Img variant="top" src={truck_img} />
+                        <Card.Body>
+                            <Card.Title>Transportation</Card.Title>
+                            <Card.Text>
+                            CO2: {product[0].rating_data.CO2}
+                            </Card.Text>
+                            <Button variant="primary">Learn more</Button>
+                        </Card.Body>
+                        </Card>
+
+                        <Card style={{ width: '18rem' }} text='dark'>
+                        <Card.Img variant="top" src={truck_img} />
+                        <Card.Body>
+                            <Card.Title>Retail</Card.Title>
+                            <Card.Text>
+                            CO2: {product[0].rating_data.CO2}
+                            </Card.Text>
+                            <Button variant="primary">Learn more</Button>
+                        </Card.Body>
+                        </Card>
+                        </CardDeck>
+                    </div>
+                 : null}
         </div>
         )
     }
