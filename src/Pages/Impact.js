a57da627ddbf368d89cbcb45b43b837e15b03585
Chris Shortell <cshor028@gmail.com>
2020-07-16 11:25:34 -0400

    lets go

--- src/Pages/Impact.js
+++ src/Pages/Impact.js
@@ -17,11 +17,178 @@ class Impact extends Component {
         // Notice that return must take one container (div) but can have lots inside
         return (
         <div className="Impact">
+           
             <Navbars /> 
             <h1>Impact</h1>
+            
             <br></br>
-            Hello
+            <div className="text">
+                <p>
+                    Why is climate change such a big problem?
+                </p>
+                
+                <br>
+                                                
+                </br>
 
+                <p>
+                    In the past few decades climate change has caused
+                </p>
+                <p>
+                    -Increasing temperatures which have created worsened extreme weather.
+                </p>
+
+                <p>
+                    -Chunks of ice have melted and continue to melt not only displacing the artic species living there, but also rising the sea level.
+                </p>
+
+                <p>
+                    -Wildfire seasons are much longer.
+                </p>
+
+                <p>
+                    -Mosquitos have expanded their terroritory which has increased the spread of disease.
+                </p>
+
+                <br>
+                     
+                </br>
+
+                <p>
+                    What caused these harmful changes?
+                </p>
+
+                <br>
+                     
+                </br>
+
+                <p>
+                    -It's strongly related to humans consumption of fossil fuels and global deforestation.
+                
+                </p>
+
+                <p>
+                -Earth has already warmed by about 1 degree Celsius, or 1.8 degrees Fahrenheit, since the 19th century, before industry started to boom.
+                    </p>
+
+                    <p>
+                   -While we experience the effects, we’re on our way toward 1.5 degrees C (2.7 F) by as early as 2030.
+                     </p>
+
+
+                     <br>
+                     
+                </br>
+
+                <p>
+                     Why .5 degrees is such a big deal
+                </p>
+
+                <br>
+                                                                 
+                </br>
+
+                <p>
+                     -A warmer world by a half-degree Celsius — has more evaporation, leading to more water in the atmosphere.
+                     Such changing conditions put our agriculture, health, water supply and more at risk.
+                     </p>
+
+
+                     <p>
+                     -The increased evaporation and additional moisture to the atmosphere has led to 30% more intense rain during heavy downpours in that part of the U.S.                                         
+                </p>
+
+
+                <p>
+                    
+                        </p>
+
+                <br>
+                     
+                        </br>
+
+                        <p>
+                    There's still time to act
+                        </p>
+
+
+
+                <p>
+                -Whether it’s a shift of 1.5 degrees or 2 degrees, these warming levels aren’t magic thresholds. Every rise in warming is worse for the planet than the last.
+                        </p>
+
+<p>
+                       - It's not too late to slow the pace of climate change as long as we act today. With your help, we can attack this challenge.
+</p>
+
+<p>
+                    
+                        </p>
+                        <br>
+                     
+                        </br>
+
+                        <p>
+                    What else can I do to help climate change?
+                        </p>
+
+                        <p>
+                     1. Switch to 100% green power
+
+                    </p>
+                        <p>
+Still haven’t switched to a green energy provider? Then now’s the time! Throughout the world, the use of energy represents by far the largest source of greenhouse gas emissions caused by human activity. Around two thirds of global greenhouse gas emissions are linked to burning fossil fuels for energy to be used for heating, electricity, transport and industry. And in Europe too, energy production and use, including the energy used in transport, accounts for some 80 percent of the EU's greenhouse gas emissions.
+                        </p>
+                        <p>
+2. Save Energy
+                        </p>
+                        <p>
+                        It might sound like the most original-sounding tip around, but it's as relevant as ever. Saving energy not only saves you money - it also helps to cut emissions too.
+                        </p>
+                        <p>
+                        3. Optimise your diet</p>
+                        <p> In the EU, meat and dairy production is estimated to be responsible for 12-17% of total greenhouse gas emissions, while throughout the world, the global livestock industry produces more greenhouse gas emissions than all cars, planes, trains and ships combined. That doesn't mean that everyone has to become vegan or vegetarian - even a small shift in diets, with a reduction in meat and dairy products, and more plant-based foods instead, could reduce the pressure that agriculture places on the environment.
+
+When buying fruits and vegetables, try to buy organic wherever the options (and the price) will allow. Organic foods are usually not only healthier because they contain fewer harmful substances, but growing them also protects the environment and the climate. You could even go one step further and help support local organic farmers by signing up to receive a veggie box directly from people who grow the food nearby, thus also helping cut down on the emissions caused by transporting produce throughout the world.
+                        </p>
+
+
+                        <p>
+                        4. Avoid plastic wherever you can
+                        </p>
+                        <p>
+Plastic is the all-round material par excellence and is therefore present in pretty much every aspect of our lives. But the durability of the material (which also makes it so popular) is of course also it's most drastic disadvantage: we're struggling to get rid of it. Plastic has found its way pretty much everywhere - on streets, in rivers, on the beach, in cosmetics, in waste water, in our clothing, even in the air we breathe. And there's also a close connection between climate change and our massive global plastic problem. Almost every plastic is produced from fossil fuels - and in every single phase of its life cycle, plastic emits greenhouse gases. But there are already a lot of alternatives available.
+                        </p>
+
+                        <p>
+                       5. Sharing is caring! </p>
+                        <p> If we own less and use more things collectively, we need to produce fewer things - and that saves on resources. Sharing cars, exchanging clothes, lending and borrowing tools - there are multiple different possibilities for collective consumption are they're being used by millions of people worldwide.
+                        </p>
+
+                        <p>
+                    6. Protect our forests and plant more trees</p>
+                        <p> It has long been known how important forests are both for the microclimate in individual regions and for the global climate as a whole. They "feed" on CO2 and convert the climate-damaging gas into oxygen, which is vital for our survival. A research team at ETH Zurich has compiled some fascinating figures: Two thirds of man-made CO2 emissions could be removed from our atmosphere if we were to reforest 900 million hectares of forests worldwide. Forest restoration “isn’t just one of our climate change solutions, it is overwhelmingly the top one,” said the lead scientist, climate change ecologist Tom Crowther. But we should not only focus on reforestation measures, but also stop the deforestation of huge areas at the same time. European meat production also plays a decisive role in this context. Poultry, pigs and cattle are mostly fed with soya, which has grown on Brazilian soil - and tropical rainforest was previously found on these areas. So you can make a contribution by limiting your meat consumption.
+                        </p>
+                        <p> 
+                        -
+                        </p>
+
+<p> 
+                        Sources
+                        </p>
+
+                        <p> 
+                        “This Is Why Fighting Climate Change Is so Urgent.” Environmental Defense Fund, www.edf.org/climate/why-fighting-climate-change-so-urgent.
+                        </p>
+
+                        <p>
+                        Deutschland, Fridays for Future, et al. “12 Things You Can Do Right Now on Climate Change: Climate Protection.” RESET.to, en.reset.org/act/12-things-you-can-do-climate-change-0.
+                        </p>
+
+
+
+
+        </div>
         </div>
         )
     }
@@ -29,3 +196,6 @@ class Impact extends Component {
 
 // Always need an export for others to access
 export default Impact;
+
+
+
